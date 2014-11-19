6af0869f896505a85f6e08a4f1395cce377a676c
Dan Abramov <dan.abramov@gmail.com>
2014-11-19 21:58:27 +0100

    Move stuff around for GH pages

--- scripts/constants/PayloadSources.js
+++
@@ -1,6 +0,0 @@
-var keyMirror = require('react/lib/keyMirror');
-
-module.exports = keyMirror({
-  SERVER_ACTION: null,
-  VIEW_ACTION: null
-});
--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var React = require('react'),
-    routes = require('./routes');
-
-React.render(routes, document.body);
